8cd5f40849978050a6ba3311d53782d6b5410c10
Dahausa <joernhauser@mailbox.org>
2020-11-06 20:20:40 +0000

    added logo and background to main scene

--- scenes/main.scene.js
+++ scenes/main.scene.js
@@ -1,17 +1,23 @@
 class Main extends Phaser.Scene {
 
-    constructor ()
-    {
+    constructor() {
         super({ key: 'Main', active: true });
+        this.dynamic = null
     }
 
-    preload ()
-    {
-        this.load.image('bg', 'assets/images/sky.png');
+    preload() {
+        this.load.image('background', 'assets/images/background.png');
+        this.load.image('logo', 'assets/images/logo.png');
     }
 
-    create ()
-    {
-        this.add.image(400, 300, 'bg');
+    create() {
+        this.add.image(400, 300, 'background');
+        let startButton = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY,'logo').setInteractive()
+
+        startButton.once('pointerup', this.startGame, this);
+    }
+
+    startGame() {
+        console.log("Call next scene")
     }
 }
