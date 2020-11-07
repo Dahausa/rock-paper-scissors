2b5039b5630c0a7adea0af228ce4b19ef6d54b87
Dahausa <joernhauser@mailbox.org>
2020-11-07 19:27:49 +0000

    Changed logo

--- scenes/main.scene.js
+++ scenes/main.scene.js
@@ -12,9 +12,16 @@ class Main extends Phaser.Scene {
 
     create() {
         this.add.image(400, 300, 'background');
-        let startButton = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY,'logo').setInteractive()
-
+        let startButton = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'logo').setInteractive()
         startButton.once('pointerup', this.startGame, this);
+
+        let style = { font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+
+        let text = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY - 100, 'You know the rules!',style);
+
+
+
+        text.setShadow(0, 0, 'rgba(0, 0, 0, 0.5)', 0);
     }
 
     startGame() {
